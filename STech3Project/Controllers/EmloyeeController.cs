f4724e81990065960b6a043be5e03e13fd7f4674
Strange3228 <51462607+Strange3228@users.noreply.github.com>
2020-06-07 19:15:35 +0200

    Add project files.

---
+++ STech3Project/Controllers/EmloyeeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using STech3Project.Models;
+
+namespace STech3Project.Controllers
+{
+    public class EmloyeeController : Controller
+    {
+        // GET: Emloyee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<Employee> empList = db.Employee.ToList<Employee>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            return View(new Employee());
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.Employee.Add(emp);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
