60114644729a92bf5675a6120868e5abe2002485
Strange3228 <51462607+Strange3228@users.noreply.github.com>
2020-06-09 00:54:45 +0200

    Editing database

--- STech3Project/Controllers/EmloyeeController.cs
+++ STech3Project/Controllers/EmloyeeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,7 +28,15 @@ namespace STech3Project.Controllers
         [HttpGet]
         public ActionResult AddOrEdit(int id = 0)
         {
-            return View(new Employee());
+            if(id==0)
+                return View(new Employee());
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+                    return View(db.Employee.Where(x => x.EmployeeID==id).FirstOrDefault<Employee>());
+                }
+            }
         }
 
         [HttpPost]
@@ -35,10 +44,21 @@ namespace STech3Project.Controllers
         {
             using (DBModel db = new DBModel())
             {
-                db.Employee.Add(emp);
-                db.SaveChanges();
-                return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                if (emp.EmployeeID == 0)
+                {
+                    db.Employee.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                }
             }
-        }
+        } 
+
+
     }
 }
