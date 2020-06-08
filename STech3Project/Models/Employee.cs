f13481b746e1d0808f9e2348bcfb5d9b1548bf4c
Strange3228 <51462607+Strange3228@users.noreply.github.com>
2020-06-08 20:49:05 +0200

    Make required fields

--- STech3Project/Models/Employee.cs
+++ STech3Project/Models/Employee.cs
@@ -11,11 +11,14 @@ namespace STech3Project.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Employee
     {
         public int EmployeeID { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string Position { get; set; }
         public string Office { get; set; }
         public Nullable<int> Age { get; set; }
